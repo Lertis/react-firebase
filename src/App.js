55d2a14a245061abe2f4b580627ee4c9dcae4b8b
Yura Husak <yura.husak@juriba.com>
2020-03-29 19:50:23 +0300

    Added simple navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Navbar } from './components/Navbar';
 
 export default function App() {
 	return (
-		<div className="container">
-			<h1>Header here!</h1>
+		<BrowserRouter>
+		<Navbar />
+		<div className="container pt-4">
+			<Switch>
+				<Route path={'/'} exact component={Home}></Route>
+				<Route path={'/about'} exact component={About}></Route>
+			</Switch>
 		</div>
+		</BrowserRouter>
 	);
 }
